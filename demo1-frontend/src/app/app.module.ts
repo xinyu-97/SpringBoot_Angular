45d6a775eb3e9b62a96148aea8de98f0b1c925ae
Xinyu Chen <xinyuchen2769@gmail.com>
2020-04-13 22:53:03 -0400

    created new component

--- demo1-frontend/src/app/app.module.ts
+++ demo1-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule
